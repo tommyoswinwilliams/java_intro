b3aeebb05b8c5fcda61c8156aad96108e895647f
Tommy Oswin Williams <tommy@teow.co.uk>
2021-10-25 15:55:49 +0100

    PriceDisplay class

---
+++ PriceDisplay.java
@@ -0,0 +1,7 @@
+public class PriceDisplay{
+  public static void main(String[] args) {
+    String item = "Coffee Machine";
+    Double price = 15.99;
+    System.out.printf("The price of the %s is £%.2f", item, price);
+  }
+}
