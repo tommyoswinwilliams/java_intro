458fba57002fd86f8ff7be8c575ee41bd0c314b2
Tommy Oswin Williams <tommy@teow.co.uk>
2021-10-25 15:13:27 +0100

    Hello world, calculator and clock

---
+++ Clock.java
@@ -0,0 +1,11 @@
+import java.time.format.DateTimeFormatter;
+import java.time.LocalTime;
+
+
+public class Clock {
+  public static void main(String[] args) {
+    LocalTime time = LocalTime.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+    System.out.printf("The current time is %s \n", time.format(formatter));
+  }
+}
